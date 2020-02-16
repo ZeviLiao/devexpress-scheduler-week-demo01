84cfe8bfeccd0a2e838e549106cb042527eed4bc
Zevi Liao <jointjm@gmail.com>
2020-02-16 14:38:16 +0800

    control hours

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
+import * as React from 'react'
+import Paper from '@material-ui/core/Paper'
+import { ViewState } from '@devexpress/dx-react-scheduler'
 import {
   Scheduler,
   WeekView,
@@ -8,37 +8,40 @@ import {
   DateNavigator,
   Appointments,
   TodayButton,
-} from '@devexpress/dx-react-scheduler-material-ui';
+} from '@devexpress/dx-react-scheduler-material-ui'
 
-import { appointments } from './demo-data/appointments';
+import { appointments } from './demo-data/appointments'
 
 export default class Demo extends React.PureComponent {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       data: appointments,
-    };
+      currentDate: '2020-02-16',
+    }
+    this.currentDateChange = currentDate => {
+      this.setState({ currentDate })
+    }
   }
 
   render() {
-    const { data } = this.state;
+    const { data, currentDate } = this.state
 
     return (
       <Paper>
-        <Scheduler
-          data={data}
-        >
+        <Scheduler data={data} height={660}>
           <ViewState
-            defaultCurrentDate="2020-02-16"
+            currentDate={currentDate}
+            onCurrentDateChange={this.currentDateChange}
           />
-          <WeekView />
+          <WeekView startDayHour={9} endDayHour={19} />
           <Toolbar />
           <DateNavigator />
           <TodayButton />
           <Appointments />
         </Scheduler>
       </Paper>
-    );
+    )
   }
 }
