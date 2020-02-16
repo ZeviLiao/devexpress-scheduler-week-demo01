0798348b1806394b724b707b757e3f594715fb51
Zevi Liao <jointjm@gmail.com>
2020-02-16 13:43:02 +0800

    weekly view

---
+++ src/components/calendar/demo-data/appointments.js
@@ -0,0 +1,12 @@
+export const appointments = [
+  {
+    startDate: '2018-10-31 10:00',
+    endDate: '2018-10-31 11:00',
+    title: 'Meeting',
+  },
+  {
+    startDate: '2018-11-01 18:00',
+    endDate: '2018-11-01 19:30',
+    title: 'Go to a gym',
+  },
+]
